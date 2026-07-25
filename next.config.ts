7cddc48bb69364ca8907b4fbd8a277e61fd7a316
delma60 <oladeleofficial@gmail.com>
2026-07-25 21:35:45 +0100

    ioko

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  env: {
+    APP_URL: process.env.APP_URL ?? "https://wallet.spurs.com.ng",
+    SPURS_ISSUER: process.env.SPURS_ISSUER ?? "https://accounts.spurs.com.ng",
+    SPURS_SESSION_SECRET: process.env.SPURS_SESSION_SECRET,
+  },
 };
 
 export default nextConfig;
